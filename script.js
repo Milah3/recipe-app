0fa45a71476c2f34ab7515e205319b46285b920c
Cigomba Miler <Cigombamiler@gmail.com>
2021-10-05 17:49:51 -0400

    first commit of recipe-app

---
+++ script.js
@@ -0,0 +1,12 @@
+async function getRandomMeal() {
+   const randomMeal = await fetch('www.themealdb.com/api/json/v1/1/random.php');
+   randomMeal.
+}
+
+async function getMealById(id) {
+   const meal = await fetch('www.themealdb.com/api/json/v1/1/lookup.php?i=' + id);
+}
+
+async function getMealsBySearch(term) {
+   const meals = await fetch(`www.themealdb.com/api/json/v1/1/search.php?s=${term}`);
+}
