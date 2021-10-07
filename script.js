7212820af495a3ed4882fcd74bc0d8f98cd4e52e
Cigomba Miler <Cigombamiler@gmail.com>
2021-10-07 17:13:04 -0400

    tired of this. toggle heart function not working atm. will try again later

--- script.js
+++ script.js
@@ -1,98 +1,82 @@
-async function getRandomMeal() {
-   const resp = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+getMealsBySearch('cake');
+// getRandomMeal();
 
-   const respData = await resp.json();
-   randomMeal = respData.meals[0];
-   // console.log(randomMeal);
-   addMeal(randomMeal, true);
+async function getRandomMeal() {
+  const resp = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/random.php"
+  );
+
+  const respData = await resp.json();
+  randomMeal = respData.meals[0];
+  addMeal(randomMeal, true);
 }
 
 async function getMealById(id) {
-   const resp = await fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id);
-   const respData = await resp.json();
+  const resp = await fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id);
 
-   console.log(respData);
-
-   addMeal(idMeal, false)
+  const respData = await resp.json();
+  console.log(respData);
+  addMeal(idMeal, false);
 }
 
-getMealsBySearch('Chicken');
-
 async function getMealsBySearch(term) {
-   const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`);
+  const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`);
+  const resData = await res.json();
+  const meals = resData.meals;
+  const mealsEl = document.getElementById("meals");
 
-   const meals = await res.json();
-
-   // console.log(meals);
-   console.log(Array.from(meals).forEach(meal => console.log(meal.strMeal) ))
-
-   // const output = '';
-   // const mealsEl = document.getElementById('meals');
-
-   // const mealsArray = [meals];
-
-   // meals.forEach(meal => {
-   //    const mealEl = document.createElement('div');
-   //    mealEl.classList.add('meal');
-   //    mealEl.innerHTML = `<div class="meal-header"> 
-   //    <span class="random"> Random Recipe </span>
-   //    <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
-   //    </div>
-   //    <div class="meal-body">
-   //        <h4>${meal.strMeal}</h4>
-   //        <button class="fav-btn">
-   //            <i class="fas fa-heart"></i>
-   //        </button>
-   //    </div> `
-   //    mealsEl.appendChild(mealEl)
-   // });
-   // mealsEl.innerHTML += output;
-
-
-
+  meals.forEach((meal) => {
+    addMeal(meal, false);
+  });
 }
 
 function addMeal(mealData, random = false) {
-   const mealsEl = document.getElementById('meals');
-   /*
+  const mealsEl = document.getElementById("meals");
+  /*
       Alternate way of adding meal div
                const meal = document.createElement('div');
                meal.classList.add('meal')
                meal.innerHTML = `... insert child elements here...`
    */
 
-   mealsEl.innerHTML = ` 
+  mealsEl.innerHTML += ` 
    <div class="meal">
    <div class="meal-header"> 
-   ${random ? `<span class="random"> Random Recipe </span>` : '' }
+   ${random ? `<span class="random"> Random Recipe </span>` : ""}
        <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}">
    </div>
    <div class="meal-body">
-       <h4>${mealData.strMeal}</h4>
-       <button class="fav-btn">
+        <h4>${mealData.strMeal}</h4>
+       <button class="fav-btn" id="${mealData.idMeal}" onclick="toggleHeart(${mealData.idMeal})">
            <i class="fas fa-heart"></i>
        </button>
    </div> 
-</div>`
+</div>`;
 
-   }
+}
 
+function toggleHeart() {
+    const btn = document.getElementById(`${mealData.idMeal}`);
+    btn.addEventListener('click', () => {
+        // if ()
+        btn.style.color == 'purple' ? btn.style.color = 'grey': btn.style.color = 'purple';
+    })
+}
 
+// if (i < 5) {
+//   const mealEl = document.createElement('div');
+//   mealEl.classList.add('meal');
+//   mealEl.innerHTML = `<div class="meal-header">
+//   <span class="random"> ${meal.strMeal} </span>
+//   <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+//   </div>
+//   <div class="meal-body">
+//       <h4>${meal.strMeal}</h4>
+//       <button class="fav-btn">
+//           <i class="fas fa-heart" ></i>
+//       </button>
+//   </div>`;
 
+//   mealsEl.appendChild(mealEl)
 
-
-
-
-/* <div class="meal">
-<div class="meal-header">
-    <span class="random"> Random Recipe </span>
-    <img src="https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg" alt="">
-</div>
-<div class="meal-body">
-    <h4>Veggie-t-ables</h4>
-    <button class="fav-btn">
-        <i class="fas fa-heart"></i>
-    </button>
-    
-</div> 
-</div> */
+// }
