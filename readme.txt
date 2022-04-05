f423bd8fbad8477844e71c66620ced62ca766dba
Cigomba Miler <Cigombamiler@gmail.com>
2022-04-04 21:08:32 -0700

    Done! I'm actually really proud of how it turned out. It actually looks better than I expected. And now to lets DEPLOY IT:)

---
+++ readme.txt
@@ -0,0 +1,14 @@
+> Hey, it's Miler. I'm actually really proud of how this project turned it. I was able to learn a lot about how to retreive data using the fetch and asynchronously use it in your code.
+It's pretty epic. :). Can't wait to learn more :).
+
+
+[X]: Add remove function to favorites list
+[X]: Add search functionality
+    a. [X]: Add a search not found page
+[X]: Add text-overflow catch
+[X]: Navbar mobile compatibility
+[]: Add about us page
+[]: Add pop-up modal for each searched item, depending on ID!
+    []: Hover text before click to open modal
+[]: Deploy!!! :)
+
--- script.js
+++ script.js
@@ -1,43 +1,67 @@
-let fav_div = document.querySelector(".fav-container");
-let fav_class = document.querySelector(".fav-meals");
+const fav_div = document.querySelector(".fav-container");
+const fav_class = document.querySelector(".fav-meals");
+const mealPopup = document.querySelector("#meal-container-popup");
+const mealInfoEl = document.querySelector("#meal-info");
 
-function main() {
+function __init() {
 	if (performance.navigation.TYPE_RELOAD == performance.navigation.type) {
 		localStorage.clear();
 		console.log("Page reloaded, storage cleared()");
 	}
 	fav_div.style.display = "none";
 
-	// getRandomMeal();
+	getMealsBySearch("cake").then(x => {
+		x.forEach(y => {
+			addMeal(y, false);
+		});
+	});
+
+	searchMeals();
+	// showMealInfo(52900);
 }
 
-main();
-searchMeals();
+mealPopup.style.display = "none";
+__init();
+
+function refreshRandom() {
+	const x = document.querySelector("#meals");
+
+	getRandomMeal();
+}
 
 async function searchMeals() {
 	const searchBtn = document.getElementById("search_btn");
 	const searchInput = document.getElementById("search_input");
 
-	searchInput.addEventListener("keypress", event => {
-		if (event.key === "Enter") {
-			event.preventDefault();
-			searchBtn.click();
-		}
-	});
+	let mealsContainer = document.querySelector("#meals");
+	let subText = document.querySelector("#browse_text");
 
 	searchBtn.addEventListener("click", async event => {
 		const value = searchInput.value;
 
-		const meals_data = await getMealsBySearch(value);
+		let mealsData = await getMealsBySearch(value);
 
-		if (meals_data.length > 0) {
-			document.querySelector("#meals").innerHTML = "";
+		// subText.innerHTML = "Yellow Mellow";
+		mealsContainer.innerHTML = ""; //clear previous meals
+		// fav_class.innerHTML = "";
+		localStorage.clear();
 
-			meals_data.forEach(meal => {
+		if (mealsData) {
+			subText.innerHTML = `Showing results for '${value}'`;
+			mealsData.forEach(meal => {
 				addMeal(meal, false);
-			});
+			}); //populate with new meals from search
 		} else {
-			document.querySelector("#browse_text").innerText = `No results found for '${value}'`;
+			subText.innerText = `No results found for '${value}'`;
+			mealsContainer.innerHTML = " "; //clear previous meals
+			document.querySelector(".randomBtn").style.display = "block";
+		}
+	});
+
+	searchInput.addEventListener("keypress", event => {
+		if (event.key === "Enter") {
+			event.preventDefault();
+			searchBtn.click();
 		}
 	});
 }
@@ -78,8 +102,8 @@ function addMeal(mealData, random = false) {
 
 	mealsEl.innerHTML += `
    <div class="meal-container">
-   ${random ? `<span class="random"> Random Recipe </span>` : ""}
-       <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}">
+   ${random ? `<span id="random"> Random Recipe </span>` : ""}
+       <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}" onclick="showMealInfo(${mealData.idMeal})">
       <div class="meal-body">
         <h4>${mealData.strMeal}</h4>
         <button class="fav-btn" id="${mealData.idMeal}" onclick="toggleHeart(${mealData.idMeal})">
@@ -145,11 +169,16 @@ function displayMealInFavs(mealId) {
 		const del_btn = document.createElement("button");
 
 		meal_doc.id = `fav_${mealId}`;
-		del_btn.classList = "del_btn";
+		del_btn.classList = "del_btn fas fa-close";
 
 		meal_spn.innerText = meal.strMeal;
 		meal_img.src = meal.strMealThumb;
-		del_btn.innerText = "X";
+		// del_btn.innerText = "X";
+
+		meal_img.addEventListener("click", () => {
+			e.preventDefault();
+			showMealInfo(mealId);
+		});
 
 		del_btn.addEventListener("click", () => {
 			toggleHeart(mealId);
@@ -172,6 +201,59 @@ function removeFromFavDiv(id) {
 
 	// console.log("Element: ", elToBeRemoved, " has been removed from Favorites");
 }
+
+async function showMealInfo(mealId) {
+	mealData = await getMealById(mealId);
+	mealData = mealData[0];
+	mealPopup.style.display = "flex"; // show popup
+
+	mealInfoEl.innerHTML = ` <button id="close-popup"><i class="fas fa-close"></i></button>
+	<h1>${mealData.strMeal}</h1>
+	<div class="tuple">
+	<img class="popup-img" src="${mealData.strMealThumb}">
+	<ul id="recipes_list">
+		<h2>Ingredients:</h2>
+	
+	</ul>
+	</div>
+	<p>${mealData.strInstructions}</p>`;
+
+	const popupCloseBtn = document.querySelector("#close-popup");
+	let ingredientsEl = document.querySelector("#recipes_list");
+
+	// filter mealData for ingredients only
+	const mealArr = Object.entries(mealData);
+	const ingredients = mealArr.filter(ingredient => {
+		return ingredient[0].match("strIngredient") && ingredient[1] !== "" ? ingredient[1] : null;
+	});
+
+	// console.log(ingredients);
+
+	ingredients.forEach(ingredient => {
+		let li = document.createElement("li");
+		li.innerText = ingredient[1];
+		ingredientsEl.appendChild(li);
+	});
+
+	popupCloseBtn.addEventListener("click", e => {
+		e.preventDefault();
+		// console.log("e", e.key);
+		mealPopup.style.display = "none";
+	});
+}
+
+// mealPopup.innerHTML = `
+// <button id="close-popup"><i class="fas fa-close"></i></button>
+// <h1>Meal title</h1>
+// <img src="">
+// <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio aspernatur voluptatem totam
+// 	recusandae natus. Tempora totam amet consectetur aliquam pariatur.</p>
+// <ul>
+// 	<li>ing 1 / measure</li>
+// 	<li>ing 2 / measure</li>
+// 	<li>ing 3 / measure</li>
+// </ul>
+// `;
 
 // Code for listing fewer meals
 // if (i < 5) {
